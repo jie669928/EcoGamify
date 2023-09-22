87ab97b7d8710b9c4e76b499ada874bfcca73d5e
jie66 <jie669928@gmail.com>
2023-09-22 22:20:55 +0800

    2220.22092023.SJ

--- app/src/main/java/com/example/ecogamify/LoginActivity.kt
+++ app/src/main/java/com/example/ecogamify/LoginActivity.kt
@@ -1,11 +1,23 @@
 package com.example.ecogamify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ecogamify.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login2)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener{
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+        binding.tvHaventAccount.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/ecogamify/MainActivity.kt
+++ app/src/main/java/com/example/ecogamify/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.ecogamify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ecogamify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        binding.btnRegister.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
